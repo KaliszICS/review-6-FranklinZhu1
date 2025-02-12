959aec4f145e78fd7d059cfdd6b669c1b2af4cbf
franklin-zhu-ics4u1-02-25 <123124410+FranklinZhu1@users.noreply.github.com>
2025-02-12 14:55:45 +0000

    Create solution methods

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,41 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+
+		public static int sum(int a, int b) {
+			return a + b;
+		}
+
+		public static int difference(int a, int b) {
+			return a - b;
+		}
+
+		public static double product(double a, double b) {
+			return a*b;
+		}
+
+		public static String removeFirst(String s) {
+			return s.substring(1);
+		}
+
+		public static int combinedLength(String a, String b) {
+			return a.length() + b.length();
+		}
+
+		public static boolean isEven(int a) {
+			return a%2 == 0;
+		}
+
+		public static boolean isOdd(int a) {
+			return a%2 != 0;
+		}
+
+		public static boolean isPositive(int a) {
+			return a > 0;
+		}
+
+		public static boolean isNegative(int a) {
+			return a < 0;
+		}
 
 	}
 
-}
